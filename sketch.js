bf19bed4cf497c73b4ea4e5af258fff3e28eedff
lociobello <lorenzo2.bernini@mail.polimi.it>
2021-09-16 20:35:52 +0200

    na roba

--- sketch.js
+++ sketch.js
@@ -4,10 +4,33 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  background("black");
+  stroke("white");
+  strokeWeight(1);
 }
 
 function draw() {
-  // put drawing code here
-  background("black");
+  push();
+  stroke("white");
+  strokeWeight(4);
+  translate(frameCount * 2, 0);
+  line(0, mouseY, 0, windowHeight - mouseY);
+  stroke("black");
+  line(0, mouseY + 0.2 * mouseY, 0, windowHeight - mouseY - 0.2 * mouseY);
+  line(0, windowHeight / 10, windowWidth, windowHeight / 10);
+  pop();
+  stroke("white");
+  strokeWeight(0.5);
+  line(0, (windowHeight / 10) * 2, windowWidth, (windowHeight / 10) * 2);
+  line(0, (windowHeight / 10) * 3, windowWidth, (windowHeight / 10) * 3);
+  line(0, (windowHeight / 10) * 4, windowWidth, (windowHeight / 10) * 4);
+  line(0, (windowHeight / 10) * 5, windowWidth, (windowHeight / 10) * 5);
+  line(0, (windowHeight / 10) * 6, windowWidth, (windowHeight / 10) * 6);
+  line(0, (windowHeight / 10) * 7, windowWidth, (windowHeight / 10) * 7);
+  line(0, (windowHeight / 10) * 8, windowWidth, (windowHeight / 10) * 8);
+  line(0, (windowHeight / 10) * 9, windowWidth, (windowHeight / 10) * 9);
+  line(0, (windowHeight / 10) * 10, windowWidth, (windowHeight / 10) * 10);
+  stroke("red");
+  translate(frameCount * 2, 0);
+  line(0, mouseY, 0, windowHeight - mouseY);
 }
